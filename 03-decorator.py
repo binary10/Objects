43ed864bc7ede68e66a36d2e936ab5e9b678a230
Dan Kowalczyk <binary10@users.noreply.github.com>
2016-05-07 12:05:57 -0700

    Replace print statements with logging

--- 03-decorator.py
+++ 03-decorator.py
@@ -1,34 +1,47 @@
+import logging
+import sys
+
 class Beverage():
-		pass
+	def __init__(self):
+		self.log = logging.getLogger(__name__)
+		self.log.addHandler(logging.StreamHandler())
+		self.log.setLevel(logging.INFO)
+
 
 class HouseBlend(Beverage):
 	def __init__(self):
-		pass
+		super().__init__()
 	
 	def get_description(self):
 		return 'I\'m a House Blend'
 		
 	def cost(self):
-		print('House Blend: $2.75')
+		self.log.info('House Blend: $2.75')
 		return 2.75
 
 
 class CondimentDecorator(Beverage):
-	pass
+	def __init__(self):
+		self.log = logging.getLogger(__name__)
+		self.log.addHandler(logging.StreamHandler())
+		self.log.setLevel(logging.INFO)
+
 
 class Milk(CondimentDecorator):
 	def __init__(self,beverage):
+		super().__init__()
 		self.beverage = beverage
 
 	def get_description(self):
 		return self.beverage.get_description() + '\n' + 'I\'m Milk'
 		
 	def cost(self):
-		print('Milk: $0.75')
+		self.log.info('Milk: $0.75')
 		return self.beverage.cost() + 0.75
 		
 class Mocha(CondimentDecorator):
 	def __init__(self,beverage):
+		super().__init__()
 		self.beverage = beverage
 		self.description = 'I\'m Mocha\n'
 	
@@ -36,10 +49,11 @@ class Mocha(CondimentDecorator):
 		return self.beverage.get_description() + '\n' + 'I\'m Mocha'
 
 	def cost(self):
-		print('Mocha: $0.80')
+		self.log.info('Mocha: $0.80')
 		return self.beverage.cost() + 0.80
 
 class Test_Order():
+	
 	def run(self):
 		bev = HouseBlend()
 		bev = Milk(bev)
@@ -48,4 +62,6 @@ class Test_Order():
 		print(bev.cost())
 		print(bev.get_description())
 		
+#if __name__=='__main__':
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 Test_Order().run()
