bd93865232e0296744f764f6c17ca87902d791a3
Dan Kowalczyk <dankow01@noa.nintendo.com>
2016-05-25 12:38:43 -0700

    Fix dunders

--- extra-null.py
+++ extra-null.py
@@ -4,14 +4,14 @@ class Null:
     https://www.safaribooksonline.com/library/view/python-cookbook/0596001673/ch05s24.html
     """
 
-    def _ _init_ _(self, *args, **kwargs): pass
-    def _ _call_ _(self, *args, **kwargs): return self
-    def _ _repr_ _(self): return "Null(  )"
-    def _ _nonzero_ _(self): return 0
+    def __init__(self, *args, **kwargs): pass
+    def __call__(self, *args, **kwargs): return self
+    def __repr__(self): return "Null(  )"
+    def __nonzero__(self): return 0
 
-    def _ _getattr_ _(self, name): return self
-    def _ _setattr_ _(self, name, value): return self
-    def _ _delattr_ _(self, name): return self
+    def __getattr__(self, name): return self
+    def __setattr__(self, name, value): return self
+    def __delattr__(self, name): return self
 
     
 
