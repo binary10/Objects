f428e67e82093cd3ebfcccd8205f85e96c387055
Dan Kowalczyk <binary10@users.noreply.github.com>
2016-06-05 09:29:59 -0700

    Revert "Improve tests and reference passing for amp"
    This reverts commit 8a1daa196a6c73cd79278280d16347f0418d21f1.

--- 07-façade.py
+++ 07-façade.py
@@ -3,11 +3,6 @@ Makes an interface simpler
 
 Created: 10:01 PM Wednesday, June 01, 2016
 """
-import unittest
-import logging
-import sys
-
-
 class LogObject:
     def __init__(self):
         self.log = logging.getLogger('.'.join([__name__, type(self).__name__]))
@@ -22,7 +17,6 @@ class HomeTheaterFacade(LogObject):
                 screen,
                 theaterlights,
                 popcornpopper):
-        super().__init__()
         self.amp            = amp
         self.tuner          = tuner
         self.dvdplayer      = dvdplayer
@@ -43,15 +37,12 @@ class HomeTheaterFacade(LogObject):
 
 # Complex system
 class Amplifier(LogObject):
-    def __init__(self, tuner, dvdplayer, cdplayer):
+    def __init__(self):
         super().__init__()
-        self.tuner      = tuner
-        tuner.amp = self
-        self.dvdplayer  = dvdplayer
-        dvdplayer.amp = self
-        self.cdplayer   = cdplayer
-        cdplayer.amp = self
-        
+        self.tuner = Tuner(self)
+        self.dvdplayer = DVDPlayer(self)
+        self.cdplayer = CDPLayer(self)
+
     def on(self):                   self.log.info('')
     def off(self):                  self.log.info('')
     def set_cd(self):               self.log.info('')
@@ -62,6 +53,10 @@ class Amplifier(LogObject):
 
 
 class Tuner(LogObject):
+    def __init__(self, amplifier = None):
+        super().__init__()
+        self.amplifier = amplifier
+
     def on(self):                   self.log.info('')
     def off(self):                  self.log.info('')
     def set_am(self):               self.log.info('')
@@ -70,6 +65,10 @@ class Tuner(LogObject):
 
 
 class DVDPlayer(LogObject):
+    def __init__(self, amplifier = None):
+        super().__init__()
+        self.amplifier = amplifier
+
     def on(self):                       self.log.info('')
     def off(self):                      self.log.info('')
     def eject(self):                    self.log.info('')
@@ -81,6 +80,10 @@ class DVDPlayer(LogObject):
 
 
 class CDPLayer(LogObject):
+    def __init__(self, amplifier = None):
+        super().__init__()
+        self.amplifier = amplifier
+
     def on(self):       self.log.info('')
     def off(self):      self.log.info('')
     def eject(self):    self.log.info('')
@@ -147,23 +150,15 @@ class Test(unittest.TestCase):
         cls.log = AppLog().log
 
     def test_case_01(self):
-            tuner           = Tuner()
-            dvdplayer       = DVDPlayer()
-            cdplayer        = CDPLayer()
-            amplifier       = Amplifier(tuner, dvdplayer, cdplayer)
-            screen          = Screen()
-            popcornpopper   = PopcornPopper()
-            theaterlights   = TheaterLights()
-            projector       = Projector()
-            h = HomeTheaterFacade(tuner         
-                              ,dvdplayer     
-                              ,cdplayer      
-                              ,amplifier     
-                              ,screen        
-                              ,popcornpopper 
-                              ,theaterlights 
-                              ,projector)    
-            
+            component = Tuner()
+            component = DVDPlayer()
+            component = CDPLayer()
+            component = Amplifier()
+            component = Screen()
+            component = PopcornPopper()
+            component = TheaterLights()
+            component = Projector()
+        
 
     def test_case_02(self):
         self.log.info('')          
